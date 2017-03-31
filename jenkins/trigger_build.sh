cbd38ccbc48e86e3c2417cc66899a5cd1b112252
Emil Madsen <emil@magenta.dk>
2017-03-31 11:41:13 +0200

    Let trigger_build be called from wherever

--- jenkins/trigger_build.sh
+++ jenkins/trigger_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SERVER_URL=$(cat JENKINS_SERVER_URL)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SERVER_URL=$(cat "$DIR/JENKINS_SERVER_URL")
 REPO=$(git remote -v | grep "(fetch)" | cut -f2 | cut -f1 -d' ')
 
 curl "${SERVER_URL}/git/notifyCommit?url=${REPO}"
